ea12dd0eeeef5609cf180dc4db3f605f7ca071da
elabbassi56 <158352759+elabbassi56@users.noreply.github.com>
2024-04-14 12:50:53 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -144,7 +144,7 @@ mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
 
-    conn.sendMessage(m.chat, {audio: { url: 'https://files.catbox.moe/y5qnp5.mp3'​ }, mimetype:'audio/mpeg', ptt:true }, {quoted:m})
+    
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
